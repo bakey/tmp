f29c0a30c6b5d893f0001e579328626e7cb49504
bakey <bakey1985@qq.com>
2012-08-27 21:23:50 +0800

    update

--- libedu/protected/modules/teach/models/CourseEdition.php
+++ libedu/protected/modules/teach/models/CourseEdition.php
@@ -1,21 +1,19 @@
 <?php
 
 /**
- * This is the model class for table "tbl_course".
+ * This is the model class for table "tbl_course_edition".
  *
- * The followings are the available columns in table 'tbl_course':
+ * The followings are the available columns in table 'tbl_course_edition':
  * @property integer $id
- * @property integer $edition_id
  * @property string $name
  * @property string $description
- * @property integer $view_count
  */
-class Course extends CActiveRecord
+class CourseEdition extends CActiveRecord
 {
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @param string $className active record class name.
-	 * @return Course the static model class
+	 * @return CourseEdition the static model class
 	 */
 	public static function model($className=__CLASS__)
 	{
@@ -27,7 +25,7 @@ class Course extends CActiveRecord
 	 */
 	public function tableName()
 	{
-		return 'tbl_course';
+		return '{{course_edition}}';
 	}
 
 	/**
@@ -38,12 +36,11 @@ class Course extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('edition_id, view_count', 'numerical', 'integerOnly'=>true),
-			array('name', 'length', 'max'=>255),
-			array('description', 'safe'),
+			array('name', 'required'),
+			array('name, description', 'length', 'max'=>255),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, edition_id, name, description, view_count', 'safe', 'on'=>'search'),
+			array('id, name, description', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -55,7 +52,7 @@ class Course extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-				'edition'=>array(self::HAS_ONE , 'CourseEdition','edition_id'),
+				//'edition_item'=>array(self::HAS_MANY,'EditionItem','edition'),
 		);
 	}
 
@@ -66,10 +63,8 @@ class Course extends CActiveRecord
 	{
 		return array(
 			'id' => 'ID',
-			'edition_id' => 'Edition',
 			'name' => 'Name',
 			'description' => 'Description',
-			'view_count' => 'View Count',
 		);
 	}
 
@@ -85,10 +80,8 @@ class Course extends CActiveRecord
 		$criteria=new CDbCriteria;
 
 		$criteria->compare('id',$this->id);
-		$criteria->compare('edition_id',$this->edition_id);
 		$criteria->compare('name',$this->name,true);
 		$criteria->compare('description',$this->description,true);
-		$criteria->compare('view_count',$this->view_count);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
